53ee5a2e5a5458ae0f7336d73a55fb69859ad4a5
Philipp Schuster <phip1611@gmail.com>
2020-01-18 12:20:33 +0100

    initial spring service

---
+++ service/src/main/java/de/phip1611/hackathon/hackathonbackend/HackathonBackendApplication.java
@@ -0,0 +1,13 @@
+package de.phip1611.hackathon.hackathonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBackendApplication.class, args);
+	}
+
+}
