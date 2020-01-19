7f62aaf7edbc52eead7ba7aa2d3d078a8c90c63b
Philipp Schuster <phip1611@gmail.com>
2020-01-19 22:14:03 +0100

    removed spring service and maven

--- service/src/main/java/de/phip1611/hackathon/repository/GameRepo.java
+++
@@ -1,9 +0,0 @@
-package de.phip1611.hackathon.repository;
-
-import de.phip1611.hackathon.domain.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface GameRepo extends JpaRepository<Game, UUID> {
-}
