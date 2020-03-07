c8122fbfbb99ffa3d6fdbf5e04ccf0fc8e491c0a
Philipp Schuster <phip1611@gmail.com>
2020-03-07 12:34:12 +0100

    introduced reconnect-events + readme update

--- common-ts/socket-events.ts
+++ common-ts/socket-events.ts
@@ -4,15 +4,24 @@ export interface SocketEvent {
 }
 
 export enum SocketEventType {
+    // creates game with ID
     GM_CREATE_GAME = 'GM_CREATE_GAME',
+    // reconencts to existing game after connection lost
+    GM_RECONNECT = 'GM_RECONNECT',
+    // enables buzzer for all players of game
     GM_ENABLE_BUZZER = 'GM_ENABLE_BUZZER',
+    // disables buzzer for all players of game
     GM_START_NEXT_ROUND = 'GM_START_NEXT_ROUND',
+
     // when a client enters the session
     PLAYER_HELLO = 'PLAYER_HELLO',
+    PLAYER_RECONNECT = 'PLAYER_RECONNECT',
     // when a client submitted its user name
     PLAYER_REGISTER = 'PLAYER_REGISTER',
     // when a client hit the buzzer button
     PLAYER_BUZZER = 'PLAYER_BUZZER',
+
+    SERVER_CONFIRM = 'SERVER_CONFIRM'
 }
 
 /**
@@ -29,6 +38,10 @@ export type GmEnableBuzzerPayload = void;
  */
 export type GmStartNextRoundPayload = void;
 /**
+ * Send the UUID the server has given us to reconnect to an existing game.
+ */
+export type GmReconnectPayload = string;
+/**
  * Notify game master a player registered.
  */
 export type PlayerRegisterPayload = string;
@@ -41,10 +54,21 @@ export type PlayerBuzzerPayload = string;
  * the client wants to join.
  */
 export type PlayerHelloPayload = string;
+/**
+ * Send the UUID the server has given us to reconnect to an existing game.
+ */
+export type PlayerReconnectPayload = string;
+/**
+ * UUID server sends to either player or game master to make reconnects possible.
+ */
+export type ServerConfirmPayload = string;
 
 export type SocketPayload = GmCreateGamePayload |
     GmEnableBuzzerPayload |
     GmStartNextRoundPayload |
+    GmReconnectPayload |
     PlayerRegisterPayload |
     PlayerHelloPayload |
-    PlayerBuzzerPayload;
+    PlayerBuzzerPayload |
+    PlayerReconnectPayload |
+    ServerConfirmPayload;
