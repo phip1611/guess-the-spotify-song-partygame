f9a35dbf47c3fbcebfb34650a32de8bcbf4b3fc6
Philipp Schuster <phip1611@gmail.com>
2026-04-17 09:57:15 +0200

    chore(ui): modernize unit test tooling

---
+++ ui/src/app/app.component.spec.ts
@@ -0,0 +1,7 @@
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('creates the root component', () => {
+    expect(new AppComponent()).toBeTruthy();
+  });
+});
